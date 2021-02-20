45e90247d6976a4f08bc56f18ec073650fdd7a94
davood <davoodfaezian@gmail.com>
2021-02-20 14:55:56 +0330

    Done

---
+++ DevCard_Mvc/Controllers/HomeController.cs
@@ -0,0 +1,33 @@
+﻿using System.Diagnostics;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using DevCard_Mvc.Models;
+
+namespace DevCard_Mvc.Controllers
+{
+    public class HomeController : Controller
+    {
+      
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+           
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+        public IActionResult Contact()
+        {
+            return View();
+        }
+        
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
---
+++ DevCard_Mvc/ViewComponents/ArticlesViewComponent.cs
@@ -0,0 +1,24 @@
+﻿using DevCard_Mvc.Models;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace DevCard_Mvc.ViewComponents
+{
+    public class ArticlesViewComponent : ViewComponent
+    {
+        public IViewComponentResult Invoke()
+        {
+            var articles = new List<Article>
+            {
+                new Article(1,"آموزش جاوا","یکی از بهترین زبان ها برای برنامه نویسی اندروید به حساب می آید.","blog-post-thumb-1.jpg"),
+                new Article(2,"آموزش طاحی سایت با bootstrap","فریمورک بوت استرپ واکنش گرایی سایت ما رو به سادگی تامین می کنه","blog-post-thumb-2.jpg"),
+                new Article(3,"آموزش  طراحی رزومه حرفه ای","فریمورک بوت استرپ واکنش گرایی سایت ما رو به سادگی تامین می کنه","blog-post-thumb-3.jpg")
+
+            };
+            return View("_Articles",articles);
+        }
+    }
+}
